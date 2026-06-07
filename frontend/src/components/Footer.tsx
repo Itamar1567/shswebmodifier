316e1bb8512f809bf77b4c957e49da87c336ea22
Itamar1567 <Itamarbaharav@gmail.com>
2026-06-07 13:36:05 -0400

    add privacy policy

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,22 @@
-import "./Footer.css"
+import "./Footer.css";
 
-function Footer(){
-    return(<div className="footer-container"></div>)
+function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-links">
+        <a
+          href="https://github.com/shs-webmodifier"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a href="/privacyPolicy.html" target="_blank" rel="noopener noreferrer">
+          Privacy Policy
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
